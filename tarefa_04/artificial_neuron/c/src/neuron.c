8c0d448d0207b90473cd7aeaf6e36649b8cdf910
Paulo Fylippe Sell <sellpaulof@gmail.com>
2023-11-17 19:10:29 -0300

    refact: neuron.c

--- tarefa_04/artificial_neuron/c/src/neuron.c
+++ tarefa_04/artificial_neuron/c/src/neuron.c
@@ -3,25 +3,26 @@
 
 #define ARRAY_SIZE 4
 
-void artificial_neuron(int *x, int *w, int size, int *value) {
-    int mac = 0;
-    
-    for (int i = 0; i < size; i++) {
-        mac += (x[i] * w[i]);
-    }
+void artificial_neuron(const int *x, const int *w, size_t size, int *value) {
+  int mac = 0;
 
-    *value = (mac > 0) ? mac : 0;
+  // multiply and accumulate
+  for (size_t i = 0; i < size; i++) {
+    mac += (x[i] * w[i]);
+  }
+
+  // ReLU
+  *value = (mac > 0) ? mac : 0;
 }
 
 int main() {
-    int x1[] = {1, 3, 5, 7};
-    int w1[] = {2, 4, 6, 8};
+  int x1[ARRAY_SIZE] = {1, 3, 5, 7};
+  int w1[ARRAY_SIZE] = {2, 4, 6, 8};
+  int value = 0;
 
-    int value = 0;
+  artificial_neuron(x1, w1, ARRAY_SIZE, &value);
 
-    artificial_neuron(x1, w1, ARRAY_SIZE, &value);
+  printf("value: %d\n", value);
 
-    printf("value: %d\n", value);
-
-    return 0;
+  return 0;
 }
