8973a2da3ccd40f7d7f50a6f09cf79d352ff07ad
Paulo Fylippe Sell <sellpaulof@gmail.com>
2023-12-06 18:48:40 -0300

    refact mac block for artificial neuron

--- tarefa_04/artificial_neuron/hw/mac.vhd
+++ tarefa_04/artificial_neuron/hw/mac.vhd
@@ -9,6 +9,14 @@ port (
   i_CLK : in std_logic;
   i_RST : in std_logic;
   i_START_MAC : in std_logic;
+  i_X0 : in std_logic_vector(N-1 downto 0);
+  i_X1 : in std_logic_vector(N-1 downto 0);
+  i_X2 : in std_logic_vector(N-1 downto 0);
+  i_X3 : in std_logic_vector(N-1 downto 0);
+  i_W0 : in std_logic_vector(N-1 downto 0);
+  i_W1 : in std_logic_vector(N-1 downto 0);
+  i_W2 : in std_logic_vector(N-1 downto 0);
+  i_W3 : in std_logic_vector(N-1 downto 0);
   o_MAC_FINISHED : out  std_logic;
   o_MAC : out std_logic_vector((N*2)-1 downto 0));
 end mac;
@@ -16,7 +24,6 @@ end mac;
 architecture mac_arch of mac is
 signal w_MAC  : signed((N*2)-1 downto 0) := (others => '0');
 signal w_MAC_FINISHED : std_logic := '0';
-signal w_MAC0, w_MAC1, w_MAC2, w_MAC3 : signed((N*2)-1 downto 0) := (others => '0');
 
 begin
   
@@ -27,10 +34,7 @@ begin
 	  w_MAC <= (others => '0');
 	elsif rising_edge(i_CLK) then
 	  if i_START_MAC = '1' then
-		 w_MAC <= (to_signed(1, 8) * to_signed(2, 8)) + 
-		              (to_signed(3, 8) * to_signed(4, 8)) + 
-						  (to_signed(5, 8) * to_signed(6, 8)) + 
-						  (to_signed(7, 8) * to_signed(8, 8));
+		 w_MAC <= signed(i_X0) * signed(i_W0) + signed(i_X1) * signed(i_W1) + signed(i_X2) * signed(i_W2) + signed(i_X3) * signed(i_W3);
 		 w_MAC_FINISHED <= '1';
 	  end if;
 	else
